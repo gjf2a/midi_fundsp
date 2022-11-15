b1ee772be9226bdcae095857d068510f85fa79b5
gjf2a <ferrer@hendrix.edu>
2022-11-15 17:31:31 -0600

    Updates.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::collections::BTreeMap;
 use anyhow::bail;
+use bare_metal_modulo::{MNum, ModNumC};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::{Device, Sample, SampleFormat, StreamConfig};
-use fundsp::hacker::{envelope, midi_hz, triangle, var};
+use fundsp::hacker::{AudioNode, envelope, midi_hz, triangle, var};
 use fundsp::prelude::{An, AudioUnit64, Tag, Var};
 use midi_msg::{ChannelVoiceMsg, MidiMsg};
 use midir::{Ignore, MidiInput, MidiInputPort};
@@ -14,16 +16,72 @@ fn main() -> anyhow::Result<()> {
     let mut midi_in = MidiInput::new("midir reading input")?;
     let in_port = get_midi_device(&mut midi_in)?;
 
-    let pitch = var(PITCH_TAG, 0.0);
-    let velocity = var(VELOCITY_TAG, 0.0);
+    let pv: PitchVelocities<8> = PitchVelocities::new();
 
-    run_output(pitch.clone(), velocity.clone());
-    run_input(pitch, velocity, midi_in, in_port)
+    run_output(pv.clone());
+    run_input(pv, midi_in, in_port)
 }
 
-fn run_input(
-    outgoing_pitch: An<Var<f64>>,
-    outgoing_velocity: An<Var<f64>>,
+macro_rules! envelop {
+    ($a:expr, $pv:ident, $i:expr) => {
+        {
+            let pitch = $pv.pitches[$i].clone();
+            let velocity = $pv.velocities[$i].clone();
+            envelope(move |_| midi_hz(pitch.value())) >> $a * envelope(move |_| velocity.value() / 127.0)
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct PitchVelocities<const N: usize> {
+    pitches: [An<Var<f64>>; N],
+    velocities: [An<Var<f64>>; N],
+    pitch2entry: BTreeMap<u8,usize>,
+    next_available: ModNumC<usize, N>
+}
+
+impl <const N: usize> PitchVelocities<N> {
+    pub fn new() -> Self {
+        PitchVelocities {
+            // Array initialization: https://stackoverflow.com/a/69756635/906268
+            pitches:  [(); N].map(|_| var(PITCH_TAG, 0.0)),
+            velocities:  [(); N].map(|_| var(VELOCITY_TAG, 0.0)),
+            pitch2entry: BTreeMap::new(),
+            next_available: ModNumC::new(0)
+        }
+    }
+
+    pub fn on(&mut self, note: u8, velocity: u8) {
+        self.pitches[self.next_available.a()].clone().set_value(note as f64);
+        self.velocities[self.next_available.a()].clone().set_value(velocity as f64);
+        self.pitch2entry.insert(note, self.next_available.a());
+        self.next_available += 1;
+    }
+
+    pub fn off(&mut self, note: u8) {
+        if let Some(index) = self.pitch2entry.remove(&note) {
+            self.velocities[index].clone().set_value(0.0);
+        }
+    }
+/*
+    pub fn create_sound(&self) -> impl AudioNode {
+        self.create_sound_help(self.pitches.len() - 1)
+    }
+
+    fn create_sound_help(&self, index: usize) -> impl AudioNode {
+        let e = envelop!(triangle(), self, index);
+        if index == 0 {
+            e
+        } else {
+            self.create_sound_help(index - 1) + e
+        }
+    }
+
+ */
+}
+
+fn run_input<const N: usize>(
+    mut pv: PitchVelocities<N>,
     midi_in: MidiInput,
     in_port: MidiInputPort,
 ) -> anyhow::Result<()> {
@@ -38,11 +96,11 @@ fn run_input(
                 match msg {
                     MidiMsg::ChannelVoice { channel:_, msg } => {
                         match msg {
-                            ChannelVoiceMsg::NoteOn { note, velocity } |
-                            ChannelVoiceMsg::NoteOff { note, velocity } => {
-                                outgoing_pitch.set_value(note as f64);
-                                outgoing_velocity.set_value(velocity as f64);
-                                println!("pitch: {} velocity: {}", outgoing_pitch.value(), outgoing_velocity.value());
+                            ChannelVoiceMsg::NoteOn { note, velocity } => {
+                                pv.on(note, velocity);
+                            }
+                            ChannelVoiceMsg::NoteOff { note, velocity:_ } => {
+                                pv.off(note)
                             }
                             _ => {}
                         }
@@ -74,31 +132,39 @@ fn get_midi_device(midi_in: &mut MidiInput) -> anyhow::Result<MidiInputPort> {
     }
 }
 
-fn run_output(incoming_pitch: An<Var<f64>>, incoming_velocity: An<Var<f64>>) {
+fn run_output<const N: usize>(pv: PitchVelocities<N>) {
     let host = cpal::default_host();
     let device = host
         .default_output_device()
         .expect("failed to find a default output device");
     let config = device.default_output_config().unwrap();
     match config.sample_format() {
-        SampleFormat::F32 => run_synth::<f32>(incoming_pitch, incoming_velocity, device, config.into()),
-        SampleFormat::I16 => run_synth::<i16>(incoming_pitch, incoming_velocity, device, config.into()),
-        SampleFormat::U16 => run_synth::<u16>(incoming_pitch, incoming_velocity, device, config.into()),
+        SampleFormat::F32 => run_synth::<f32,N>(pv, device, config.into()),
+        SampleFormat::I16 => run_synth::<i16,N>(pv, device, config.into()),
+        SampleFormat::U16 => run_synth::<u16,N>(pv, device, config.into()),
     }
 }
 
-fn run_synth<T: Sample>(
-    incoming_pitch: An<Var<f64>>,
-    incoming_velocity: An<Var<f64>>,
+// {vec![$(($s, Arc::new($f)),)*]}
+macro_rules! sum_sound {
+    ($var:ident, $($s:expr),* ) => {
+        ($( envelop!(triangle(), $var, $s) + )*)
+    }
+}
+
+fn run_synth<T: Sample, const N: usize>(
+    pv: PitchVelocities<N>,
     device: Device,
     config: StreamConfig,
 ) {
-    let sample_rate = config.sample_rate.0 as f64;
-    let mut sound = create_sound(incoming_pitch, incoming_velocity);
-    sound.reset(Some(sample_rate));
-    let mut next_value = move || sound.get_stereo();
-    let channels = config.channels as usize;
     std::thread::spawn(move || {
+        let sample_rate = config.sample_rate.0 as f64;
+        //let mut sound = envelop!(triangle(), pv, 0) | envelop!(triangle(), pv, 1);
+        //let mut sound = sum_sound!(pv, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+        let mut sound = envelop!(triangle(), pv, 0) + envelop!(triangle(), pv, 1) + envelop!(triangle(), pv, 2) + envelop!(triangle(), pv, 3) + envelop!(triangle(), pv, 4) + envelop!(triangle(), pv, 5) + envelop!(triangle(), pv, 6) + envelop!(triangle(), pv, 7);// + envelop!(triangle(), pv, 8) + envelop!(triangle(), pv, 9) + envelop!(triangle(), pv, 10) + envelop!(triangle(), pv, 11)+ envelop!(triangle(), pv, 12) + envelop!(triangle(), pv, 13) + envelop!(triangle(), pv, 14) + envelop!(triangle(), pv, 15);
+        sound.reset(Some(sample_rate));
+        let mut next_value = move || sound.get_stereo();
+        let channels = config.channels as usize;
         let err_fn = |err| eprintln!("an error occurred on stream: {err}");
         let stream = device
             .build_output_stream(
@@ -115,10 +181,8 @@ fn run_synth<T: Sample>(
     });
 }
 
-fn create_sound(incoming_pitch: An<Var<f64>>, incoming_velocity: An<Var<f64>>) -> Box<dyn AudioUnit64> {
-    Box::new(
-        envelope(move |_t| midi_hz(incoming_pitch.value())) >> triangle() * envelope(move |_t| (incoming_velocity.value() / 127.0)),
-    )
+fn create_sound(incoming_pitch: An<Var<f64>>, incoming_velocity: An<Var<f64>>) -> impl AudioUnit64 {
+    envelope(move |_t| midi_hz(incoming_pitch.value())) >> triangle() * envelope(move |_t| (incoming_velocity.value() / 127.0))
 }
 
 fn write_data<T: Sample>(
